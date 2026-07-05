1931ee35e7dfeb3eda4d05fe84d36420b9bc8cb0
HerrHenkel <hz9d7fb8v5@privaterelay.appleid.com>
2026-07-05 20:04:09 +0200

    Add El Sol researched image override

--- link-images-more.js
+++ link-images-more.js
@@ -1,5 +1,6 @@
 const moreLinkImages = {
-  "restaurant-3": "https://www.yachtcharterfleet.com/img/mi/mbb/3209449/restaurant-illeta.jpg"
+  "restaurant-3": "https://www.yachtcharterfleet.com/img/mi/mbb/3209449/restaurant-illeta.jpg",
+  "restaurant-5": "https://cdm0lfbn.cloudimg.io/v7/_images_base_/image_uploader/mallorca/photos_d3/original/el_sol_sunshine_bar_photo.jpg"
 };
 
 mapLinks.forEach(link => {
