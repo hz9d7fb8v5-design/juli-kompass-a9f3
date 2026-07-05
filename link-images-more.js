580aa6124f3ac4fd4d7c86dcad63f4c32e6abd42
HerrHenkel <hz9d7fb8v5@privaterelay.appleid.com>
2026-07-05 20:55:45 +0200

    Add Cala Del Toro image override

--- link-images-more.js
+++ link-images-more.js
@@ -4,7 +4,8 @@ const moreLinkImages = {
   "restaurant-5": "https://cdm0lfbn.cloudimg.io/v7/_images_base_/image_uploader/mallorca/photos_d3/original/el_sol_sunshine_bar_photo.jpg",
   "restaurant-6": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/65/28/ef/restaurante-playa-cala.jpg?w=700&h=400&s=1",
   "pinienstrand": "https://fincaservice-meier.de/wp-content/uploads/2019/10/Platja-D-Alcudia_Mallorca-Norden_13.jpg",
-  "cala-murada": "https://www.mallorca-momente.com/wp-content/uploads/2018/10/Mallorca-Momente-Blog-Cala-Murada-Strand-Slider-124224.jpg"
+  "cala-murada": "https://www.mallorca-momente.com/wp-content/uploads/2018/10/Mallorca-Momente-Blog-Cala-Murada-Strand-Slider-124224.jpg",
+  "strand-3": "https://lumaguide.sfo3.digitaloceanspaces.com/media/place_images/2025/06/11/google_place_ChIJR2fJjgaJlxIR9GPpp42Wipo_photo_3.jpg"
 };
 
 mapLinks.forEach(link => {
