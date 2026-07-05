b1973d288e8d6d7600d623cda529322746c2d4b5
HerrHenkel <hz9d7fb8v5@privaterelay.appleid.com>
2026-07-05 19:55:17 +0200

    Add researched restaurant image overrides

--- link-images.js
+++ link-images.js
@@ -1,6 +1,8 @@
 const researchedLinkImages = {
   "hoehle-1": "https://img0.oastatic.com/img2/75402636/600x300r/variant.jpg",
-  "hoehle-2": "https://www.palmaweekly.com/wp-content/uploads/2025/10/Coves-de-Campanet-2-1000x0-c-default.webp"
+  "hoehle-2": "https://www.palmaweekly.com/wp-content/uploads/2025/10/Coves-de-Campanet-2-1000x0-c-default.webp",
+  "restaurant-argentinier": "https://cdn.res-menu.net/la-portena-parrilla-argentina/albums-1.jpg",
+  "restaurant-2": "https://canjoandesaigo.com/wp-content/uploads/2025/04/La-Semana-Santa-en-Mallorca_-los-dulces-que-nunca-faltan-en-Can-Joan-de-SAigo-scaled.jpg"
 };
 
 mapLinks.forEach(link => {
